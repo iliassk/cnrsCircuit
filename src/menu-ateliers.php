ef7d5d3fbb2690518741e278641423df70b37e28
tbraquel <tristan.braquelaire@etu-u.bordeaux.fr>
2014-09-26 16:56:57 +0200

    frontpage : liste ateliers operationnel

---
+++ src/menu-ateliers.php
@@ -0,0 +1,21 @@
+<nav id="menu-ateliers">        
+
+    <div class="element_menu">
+        <h3>Liste des ateliers</h3>
+        <ul>
+            <?php
+            $idA_nom_list = $bdd->query('SELECT idA, nom FROM atelier');
+            
+            while ($idA_nom = $idA_nom_list->fetch())
+            {
+            ?>
+            <li><a href= <?php echo "atelier.php?" . $idA_nom['idA']?> ><?php echo $idA_nom['nom']; ?></a></li>
+            <?php
+            }
+            $idA_nom_list->closeCursor(); // Termine le traitement de la requête
+            ?>
+
+
+        </ul>
+    </div>  
+</nav>
