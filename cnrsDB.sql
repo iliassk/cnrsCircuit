2fd6a99a15eb0da53b8793a59b11fa6b145bca0d
bchalaux <bchalaux33@gmail.com>
2014-09-26 17:02:50 +0200

    DB mis a jour (2e essai)

--- cnrsDB.sql
+++ cnrsDB.sql
@@ -1,127 +1,52 @@
--- MySQL dump 10.13  Distrib 5.5.38, for debian-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: cnrs
--- ------------------------------------------------------
--- Server version	5.5.38-0ubuntu0.14.04.1
+-- Client: localhost
+-- Généré le: Ven 26 Septembre 2014 à 16:54
+-- Version du serveur: 5.5.38-0ubuntu0.14.04.1
+-- Version de PHP: 5.5.9-1ubuntu4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `atelier`
+-- Base de données: `cnrs`
 --
 
-DROP TABLE IF EXISTS `atelier`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `atelier` (
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `atelier`
+--
+
+CREATE TABLE IF NOT EXISTS `atelier` (
   `idA` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   `descriptif` varchar(500) DEFAULT NULL,
-  `idT` int(11) NOT NULL,
-  `idL` int(11) NOT NULL,
-  `idLab` int(11) NOT NULL,
+  `theme` varchar(30) NOT NULL,
+  `lieu` varchar(30) NOT NULL,
+  `labo` varchar(30) NOT NULL,
   PRIMARY KEY (`idA`),
-  UNIQUE KEY `nom` (`nom`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  UNIQUE KEY `nom` (`nom`),
+  KEY `idT` (`theme`),
+  KEY `idT_2` (`theme`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `atelier`
+-- Contenu de la table `atelier`
 --
 
-LOCK TABLES `atelier` WRITE;
-/*!40000 ALTER TABLE `atelier` DISABLE KEYS */;
-/*!40000 ALTER TABLE `atelier` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `atelier` (`idA`, `nom`, `descriptif`, `theme`, `lieu`, `labo`) VALUES
+(1, 'boucherie', NULL, '1', '1', '1'),
+(2, 'boulangerie', NULL, '1', '1', '2');
 
---
--- Table structure for table `labo`
---
-
-DROP TABLE IF EXISTS `labo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `labo` (
-  `idLab` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(30) NOT NULL,
-  PRIMARY KEY (`idLab`),
-  UNIQUE KEY `nom` (`nom`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `labo`
---
-
-LOCK TABLES `labo` WRITE;
-/*!40000 ALTER TABLE `labo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `labo` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `lieu`
---
-
-DROP TABLE IF EXISTS `lieu`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `lieu` (
-  `idL` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(30) NOT NULL,
-  PRIMARY KEY (`idL`),
-  UNIQUE KEY `nom` (`nom`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `lieu`
---
-
-LOCK TABLES `lieu` WRITE;
-/*!40000 ALTER TABLE `lieu` DISABLE KEYS */;
-/*!40000 ALTER TABLE `lieu` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `theme`
---
-
-DROP TABLE IF EXISTS `theme`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `theme` (
-  `idT` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(30) NOT NULL,
-  PRIMARY KEY (`idT`),
-  UNIQUE KEY `nom` (`nom`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `theme`
---
-
-LOCK TABLES `theme` WRITE;
-/*!40000 ALTER TABLE `theme` DISABLE KEYS */;
-/*!40000 ALTER TABLE `theme` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-09-25 17:29:02
